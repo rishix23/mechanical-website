66095d1d822612449bd32bb9c13e798ef8373a1f
rishix23 <rishi8318@gmail.com>
2024-05-07 19:34:46 -0400

    updating deployment

--- frontend/src/app/about/page.js
+++ frontend/src/app/about/page.js
@@ -7,7 +7,7 @@ function AboutPage() {
         <title>About Us</title>
       </Head>
       <h1>About Us</h1>
-      <p>This is the about page.</p>
+      <p>This is the about page</p>
     </div>
   );
 }
