62004b737ed60f9ca14813cfd1418f57c09a8b5f
rishix23 <rishi8318@gmail.com>
2024-05-07 19:01:56 -0400

    adding pages and netlify

---
+++ frontend/src/app/contact/page.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+function AboutPage() {
+  return (
+    <div>
+      <Head>
+        <title>About Us</title>
+      </Head>
+      <h1>About Us</h1>
+      <p>This is the contact page.</p>
+    </div>
+  );
+}
+
+export default AboutPage;
