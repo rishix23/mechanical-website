bad63e2dc277fd1d9a2a786f73ceef4af174098d
rishix23 <rishi8318@gmail.com>
2024-05-18 00:34:58 -0400

    updating svg

--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -40,7 +40,7 @@ export default function Navbar() {
         Photos
       </Link>
       <button className={styles.navbar__button} aria-label="Icon button" onClick={handleMenuButtonClick}>
-        <svg xmlns="(link unavailable)" fill="white" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" style={{ color: "white" }}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
         </svg>
       </button>
