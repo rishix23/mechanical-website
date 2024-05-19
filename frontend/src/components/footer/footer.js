fe91a5df704adc11b11d5921bfd7f0b64c06cd0b
rishix23 <rishi8318@gmail.com>
2024-05-18 20:03:23 -0400

    adding social media icons

--- frontend/src/components/footer/footer.js
+++ frontend/src/components/footer/footer.js
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import styles from "./footer.module.css";
 import Logo from "../../../public/favicon.ico";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSquareFacebook, faSquareTwitter, faSquareInstagram } from "@fortawesome/free-brands-svg-icons";
 
 export default function Footer() {
   return (
@@ -21,7 +23,7 @@ export default function Footer() {
             <Link href="/cooling">Cooling</Link>
           </li>
           <li>
-            <Link href="/refrigeration">Refridgeration</Link>
+            <Link href="/refrigeration">Refrigeration</Link>
           </li>
           <li>
             <Link href="/about">About Us</Link>
@@ -39,10 +41,24 @@ export default function Footer() {
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2883.662213934594!2d-122.40666305077695!3d37.785834799496076!2m3!1e0!2samerica!3e0!3m2!1sen!2sus!4v1674829137075"
           width="100%"
           height="200"
-          frameborder="0"
+          frameBorder="0"
           scrolling="no"
-          marginheight="0"
-          marginwidth="0"></iframe>
+          marginHeight="0"
+          marginWidth="0"></iframe>
+      </div>
+      <div className={styles.box}>
+        <h1>Follow Us</h1>
+        <div className={styles.socialIcons}>
+          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faSquareFacebook} className={styles.icon} />
+          </a>
+          <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faSquareTwitter} className={styles.icon} />
+          </a>
+          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faSquareInstagram} className={styles.icon} />
+          </a>
+        </div>
       </div>
     </footer>
   );
