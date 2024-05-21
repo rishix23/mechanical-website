cc6af1d8f09737967bbd553d175de7f330317f67
rishix23 <rishi8318@gmail.com>
2024-05-21 18:39:30 -0400

    updated banner

--- frontend/src/components/banner/banner.js
+++ frontend/src/components/banner/banner.js
@@ -1,11 +1,10 @@
+import React from "react";
 import styles from "./banner.module.css";
 
 const Banner = () => {
   return (
-    <div className={`${styles.banner} container`}>
-      <p>
-        Offering 24/7 Service. Call Us Anytime: <a href="tel:+1234567890">+1 (234) 567-890</a>
-      </p>
+    <div className={styles.banner}>
+      <p>Offering 24/7 Service! Call us anytime at +1234567890</p>
     </div>
   );
 };
